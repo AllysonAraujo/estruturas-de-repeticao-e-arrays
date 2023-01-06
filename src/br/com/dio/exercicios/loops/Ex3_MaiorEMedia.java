c70e23219bfdab184943b14d38f1c849da187a51
Allyson Araujo <allysoneugenio@gmail.com>
2023-01-06 17:42:21 -0300

    MaiorEMedia_2

--- src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
+++ src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
@@ -13,19 +13,22 @@ public class Ex3_MaiorEMedia {
 
         int numero;
         int maior = 0;
-
+        int soma = 0;
 
         int count = 0;
         do {
             System.out.println("Numero: ");
             numero = scan.nextInt();
 
+            soma = soma + numero;
+            
             if (numero > maior) maior = numero;
 
             count = count + 1;            
         } while (count < 5);
 
         System.out.println("Maior: " + maior);
+        System.out.println("Média : " + (soma/5));
     }
     
 }
